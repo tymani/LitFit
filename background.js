c32ed5d4c7f68a568564eba2038dc261d739776d
Ty <Ty@Tys-MacBook-Pro.local>
2018-06-22 14:00:18 -0700

    scrape text from approved sites, finds entities, stores entities

--- background.js
+++ background.js
@@ -6,3 +6,71 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab.url :
+//                 "from the extension");
+//
+//
+//     sendResponse({farewell: request.set_storage});
+//     return true;
+//     chrome.storage.sync.set(
+//       {
+//         sender.tab.url: response.category_list[0].label
+//       }
+//     );
+//     chrome.storage.sync.get(null, function(result){
+//       console.log("LOCAL STORAGE GET");
+//       console.log(result);
+//
+//     });
+//
+//
+//
+//   });
+// chrome.runtime.onConnect.addListener(function(port) {
+//   console.assert(port.name == "storage");
+//   port.onMessage.addListener(function(msg) {
+//     if (msg.storage_req != undefined) {
+//       var date = String(new Date());
+//       chrome.storage.sync.set(
+//         {
+//           date: response.category_list[0].label
+//         }
+//       );
+//       chrome.storage.sync.get(null, function(result){
+//            console.log("LOCAL STORAGE GET");
+//            console.log(result);
+//
+//       });
+//       port.postMessage({storage_resp: "Who's there?"});
+//     }
+//
+//     else if (msg.answer == "Madame")
+//       port.postMessage({question: "Madame who?"});
+//     else if (msg.answer == "Madame... Bovary")
+//       port.postMessage({question: "I don't get it."});
+//   });
+// });
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  // From content script.
+  if (sender.tab) {
+    if (request.method == "fromContentScript"){
+
+        localStorage.setItem(sender.tab.url,request.greeting); // in this case there will now be a localStorage variable called 'welcome-message' set with the value of 'hey hey!'. This will be viewable in the chrome:extensions page, click on the 'background.html / generated background.html' then view the 'Development Tools' or in Windows hit 'CTRL + SHIFT + I' and look at the 'LocalStorage' tab...
+        var resp = [];
+        var keys = Object.keys(localStorage);
+
+        // localStorage.forEach(function(element) {
+        //   resp.push(element);
+        // });
+
+      sendResponse({who: "bg",data: keys}); // this is the response sent back, 'who' tells the content page where is responding, so it can do something with the response if needed.
+        }else{
+      sendResponse({}); // snub them.
+        }
+  }
+});
--- content.js
+++ content.js
@@ -4,7 +4,108 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-      console.log(firstHref);
+      console.log($("a[href^='http']").eq(0).context.URL);
     }
   }
 );
+var content = [];
+//find url and see if it exists on list of approved websites
+//extract text
+//find out
+//google
+//AIzaSyC6sK9wlGwifnTu27vZr_liZTdgan6GoCk
+
+
+//classify url
+console.log("*****/LINK/********" + $("a[href^='http']").eq(0).context.URL);
+
+
+
+
+
+console.log("content");
+function articleExtract(callback) {
+  $.ajax({
+    headers: {
+      "X-AYLIEN-TextAPI-Application-Key": process.env.AYLIEN_APPLICATION_KEY,
+      "X-AYLIEN-TextAPI-Application-ID": process.env.AYLIEN_APPLICATION_ID
+    },
+    url:"https://api.aylien.com/api/v1/extract",
+    type:"POST",
+    data:{
+      url: String($("a[href^='http']").eq(0).context.URL)
+    },
+    success: function (response) {
+      callback(response.article);
+      console.log(response.article);
+    },
+    error: function(error) {
+      console.log("ERROR!");
+    }
+
+  });
+
+}
+
+function classifyContent(text) {
+  $.ajax({
+    url:"https://api.meaningcloud.com/class-1.1",
+    type:"POST",
+    data:{
+      key: process.env.MEANING_CLOUD_KEY,
+      of:"json",
+      model: "IPTC_en",
+      txt:text
+    },
+    success: function (response) {
+      console.log(response.category_list[0].label);
+      //add to db
+      var url_1 = String($("a[href^='http']").eq(0).context.URL);
+      var someVar = "hey hey!";
+
+      chrome.extension.sendRequest({method: "fromContentScript",greeting: someVar}, function(response) {
+
+        console.log(response.data); // response back from BG
+
+      if(response.who == 'bg'){ // checks if the response is from BG
+              //Something happened ...
+      }
+
+      var responseFromBg = response.data; // the response incase we need to use the message sent back... in this case should be 'hey yourself'
+
+
+      });
+      // var port = chrome.runtime.connect({name: "storage"});
+      // port.postMessage({storage_req: response.category_list[0].label});
+      // port.onMessage.addListener(function(msg) {
+      //   if (msg.question == "Who's there?")
+      //     port.postMessage({answer: "Madame"});
+      //   else if (msg.question == "Madame who?")
+      //     port.postMessage({answer: "Madame... Bovary"});
+      //
+      //   console.log(msg);
+      // });
+      // chrome.runtime.sendMessage({set_storage:response.category_list[0].label}, function(response) {
+      //   console.log(response.farewell);
+      // });
+      // chrome.storage.sync.set(
+      //   {
+      //     url_1: response.category_list[0].label
+      //   }
+      // );
+      // chrome.storage.sync.get(null, function(result){
+      //   console.log("LOCAL STORAGE GET");
+      //   console.log(result);
+      // });
+
+    },
+    error: function(error) {
+      console.log("ERROR!");
+    }
+
+
+  });
+}
+
+
+articleExtract(classifyContent);
