dbd5dff2b2c34b713776a2eaccd7ae49bad37e9d
Ty <Ty@Tys-MacBook-Pro.local>
2018-06-23 16:34:00 -0700

    move extract and catergorize to back-end

--- background.js
+++ background.js
@@ -1,11 +1,104 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  // From content script.
+  if (sender.tab) {
+    if (request.method == "fromContentScript"){
+
+        localStorage.setItem(sender.tab.url,request.greeting); // in this case there will now be a localStorage variable called 'welcome-message' set with the value of 'hey hey!'. This will be viewable in the chrome:extensions page, click on the 'background.html / generated background.html' then view the 'Development Tools' or in Windows hit 'CTRL + SHIFT + I' and look at the 'LocalStorage' tab...
+        var resp = [];
+        var keys = Object.keys(localStorage);
+        getKeys(extractText,sender.tab.url);
+        // localStorage.forEach(function(element) {
+        //   resp.push(element);
+        // });
+
+      sendResponse({who: "bg",data: keys}); // this is the response sent back, 'who' tells the content page where is responding, so it can do something with the response if needed.
+        }else{
+      sendResponse({}); // snub them.
+        }
+  }
 });
+var AYLIEN_APPLICATION_KEY;
+var AYLIEN_APPLICATION_ID;
+var MEANING_CLOUD_KEY;
+
+function getKeys (callback, url) {
+  $.ajax({
+    url:"api_keys.json",
+    type:"GET",
+    success: function (response) {
+      callback(response,url);
+    },
+    error: function(error) {
+      console.log("ERROR!");
+    }
+
+  });
+
+}
+
+
+function extractText(response, url) {
+  // AYLIEN_APPLICATION_KEY = response.AYLIEN_APPLICATION_KEY;
+  // AYLIEN_APPLICATION_ID = response.AYLIEN_APPLICATION_ID;
+  // MEANING_CLOUD_KEY = response.MEANING_CLOUD_KEY;
+  //
+  // console.log(AYLIEN_APPLICATION_ID);
+  // console.log(AYLIEN_APPLICATION_KEY);
+  // console.log(MEANING_CLOUD_KEY);
+
+  $.ajax({
+    headers: {
+      "X-AYLIEN-TextAPI-Application-Key": response.AYLIEN_APPLICATION_KEY,
+      "X-AYLIEN-TextAPI-Application-ID": response.AYLIEN_APPLICATION_ID
+    },
+    url:"https://api.aylien.com/api/v1/extract",
+    type:"POST",
+    data:{
+      url: url
+    },
+    success: function (resp) {
+      //callback(response.article);
+      classifyContent(response, resp.article,url);
+      console.log(resp.article);
+
+    },
+    error: function(error) {
+      console.log("ERROR!");
+    }
+
+  });
+}
+
+function classifyContent(response, text, url) {
+  $.ajax({
+    url:"https://api.meaningcloud.com/class-1.1",
+    type:"POST",
+    data:{
+      key: response.MEANING_CLOUD_KEY,
+      of:"json",
+      model: "IPTC_en",
+      txt:text
+    },
+    success: function (response) {
+      console.log(response.category_list[0].label);
+      //add to db
+
+      localStorage.setItem(url,response.category_list[0].label);
+      console.log(Object.keys(localStorage));
+    },
+    error: function(error) {
+      console.log("ERROR!");
+    }
+
+
+  });
+}
+
+
+
+
 
 
 // chrome.runtime.onMessage.addListener(
@@ -55,35 +148,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 //       port.postMessage({question: "I don't get it."});
 //   });
 // });
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  // From content script.
-  if (sender.tab) {
-    if (request.method == "fromContentScript"){
-
-        localStorage.setItem(sender.tab.url,request.greeting); // in this case there will now be a localStorage variable called 'welcome-message' set with the value of 'hey hey!'. This will be viewable in the chrome:extensions page, click on the 'background.html / generated background.html' then view the 'Development Tools' or in Windows hit 'CTRL + SHIFT + I' and look at the 'LocalStorage' tab...
-        var resp = [];
-        var keys = Object.keys(localStorage);
-
-        // localStorage.forEach(function(element) {
-        //   resp.push(element);
-        // });
-
-      sendResponse({who: "bg",data: keys}); // this is the response sent back, 'who' tells the content page where is responding, so it can do something with the response if needed.
-        }else{
-      sendResponse({}); // snub them.
-        }
-  }
-});
-
-$.ajax({
-  url:"api_keys.json",
-  type:"GET",
-  success: function (response) {
-    console.log(response.AYLIEN_APPLICATION_KEY);
-    
-  },
-  error: function(error) {
-    console.log("ERROR!");
-  }
-
-});
--- content.js
+++ content.js
@@ -15,97 +15,17 @@ var content = [];
 //google
 //AIzaSyC6sK9wlGwifnTu27vZr_liZTdgan6GoCk
 
+var curr_link = String($("a[href^='http']").eq(0).context.URL);
 
-//classify url
-console.log("*****/LINK/********" + $("a[href^='http']").eq(0).context.URL);
+chrome.extension.sendRequest({method: "fromContentScript",greeting: curr_link}, function(response) {
 
+  console.log(response.data); // response back from BG
 
-
-
-
-console.log("content");
-function articleExtract(callback) {
-  $.ajax({
-    headers: {
-      "X-AYLIEN-TextAPI-Application-Key": process.env.AYLIEN_APPLICATION_KEY,
-      "X-AYLIEN-TextAPI-Application-ID": process.env.AYLIEN_APPLICATION_ID
-    },
-    url:"https://api.aylien.com/api/v1/extract",
-    type:"POST",
-    data:{
-      url: String($("a[href^='http']").eq(0).context.URL)
-    },
-    success: function (response) {
-      callback(response.article);
-      console.log(response.article);
-    },
-    error: function(error) {
-      console.log("ERROR!");
-    }
-
-  });
-
+if(response.who == 'bg'){ // checks if the response is from BG
+        //Something happened ...
 }
 
-function classifyContent(text) {
-  $.ajax({
-    url:"https://api.meaningcloud.com/class-1.1",
-    type:"POST",
-    data:{
-      key: process.env.MEANING_CLOUD_KEY,
-      of:"json",
-      model: "IPTC_en",
-      txt:text
-    },
-    success: function (response) {
-      console.log(response.category_list[0].label);
-      //add to db
-      var url_1 = String($("a[href^='http']").eq(0).context.URL);
-      var someVar = "hey hey!";
-
-      chrome.extension.sendRequest({method: "fromContentScript",greeting: someVar}, function(response) {
-
-        console.log(response.data); // response back from BG
-
-      if(response.who == 'bg'){ // checks if the response is from BG
-              //Something happened ...
-      }
-
-      var responseFromBg = response.data; // the response incase we need to use the message sent back... in this case should be 'hey yourself'
+var responseFromBg = response.data; // the response incase we need to use the message sent back... in this case should be 'hey yourself'
 
 
-      });
-      // var port = chrome.runtime.connect({name: "storage"});
-      // port.postMessage({storage_req: response.category_list[0].label});
-      // port.onMessage.addListener(function(msg) {
-      //   if (msg.question == "Who's there?")
-      //     port.postMessage({answer: "Madame"});
-      //   else if (msg.question == "Madame who?")
-      //     port.postMessage({answer: "Madame... Bovary"});
-      //
-      //   console.log(msg);
-      // });
-      // chrome.runtime.sendMessage({set_storage:response.category_list[0].label}, function(response) {
-      //   console.log(response.farewell);
-      // });
-      // chrome.storage.sync.set(
-      //   {
-      //     url_1: response.category_list[0].label
-      //   }
-      // );
-      // chrome.storage.sync.get(null, function(result){
-      //   console.log("LOCAL STORAGE GET");
-      //   console.log(result);
-      // });
-
-    },
-    error: function(error) {
-      console.log("ERROR!");
-    }
-
-
-  });
-}
-
-
-articleExtract(classifyContent);
+});
